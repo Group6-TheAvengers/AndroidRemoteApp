7026cbfdf2bc6315bb7f2dcf599c0254298dbe76
cocohub <omid@sahragard.com>
2016-02-23 10:21:41 +0100

    Bluetooth class has new functions MainActivity is much shorter, almost all functionality has been moved to bluetooth class

--- app/src/main/java/com/sahragard/myremote/Bluetooth.java
+++ app/src/main/java/com/sahragard/myremote/Bluetooth.java
@@ -9,36 +9,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
-import org.w3c.dom.Text;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Scanner;
 import java.util.UUID;
 
 /**
  * Created by Coco on 2016-02-22.
  */
+
 public class Bluetooth {
     private Context context;
     private BluetoothAdapter adapter;
     private BluetoothDevice device;
-    private List<BluetoothDevice> foundDevices = new ArrayList<BluetoothDevice>();
-    private List<String> foundDeviceNames = new ArrayList<String>();
+    private List<BluetoothDevice> bluetoothDevices = new ArrayList<BluetoothDevice>();
+    private List<String> bluetoothDeviceNames = new ArrayList<String>();
     private Thread workerThread;
     private Thread inputThread;
     private Scanner input;
     private PrintWriter output;
+    private Activity activity;
+    private BluetoothSocket btSocket;
+    public Spinner spinner;
+    public String selectedDeviceName = "";
 
     /*
     When creating a new instance of the bluetooth class, you must enter the context.
@@ -46,9 +46,10 @@ public class Bluetooth {
     As for the bluetooth adapter, you must first declare a variable by writing:
     BluetoothAdapter adapterName = BluetoothAdapter.getDefaultAdapter(); This is your adapter that you will enter in the constructor.
     */
-    public Bluetooth(final Context context, BluetoothAdapter adapter) {
+    public Bluetooth(final Context context, BluetoothAdapter adapter, Activity activity) {
         this.context = context;
         this.adapter = adapter;
+        this.activity = activity;
 
         //Output thread
         final Handler handler = new Handler();
@@ -62,7 +63,6 @@ public class Bluetooth {
 
         //Input thread
         final Handler handler1 = new Handler();
-
         inputThread = new Thread(new Runnable() {
             public void run() {
                 while (!Thread.currentThread().isInterrupted()) {
@@ -73,22 +73,41 @@ public class Bluetooth {
         });
     }
 
+    //Enable bluetooth
+    public void enableBT() {
+        if (!adapter.isEnabled()) {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            activity.startActivityForResult(enableBluetooth, 0);
+        } else {
+            Toast.makeText(context, "Bluetooth already enabled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Disable bluetooth
+    public void disableBT() {
+        if (adapter.isEnabled()) {
+            adapter.disable();
+        } else {
+            Toast.makeText(context, "Bluetooth already disabled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     //Returns the list of found devices
-    public List<BluetoothDevice> getFoundDevices() {
-        return foundDevices;
+    public List<BluetoothDevice> getBluetoothDevices() {
+        return bluetoothDevices;
     }
 
     //Returns the list of string representations of the found devices
     //This is used to fill the spinner with items
-    public List<String> getFoundDeviceNames() {
-        return foundDeviceNames;
+    public List<String> getBluetoothDeviceNames() {
+        return bluetoothDeviceNames;
     }
 
     //Finds devices to connect to
     public void findDevices() {
         //Clears the lists in order to prevent duplicates
-        getFoundDeviceNames().clear();
-        getFoundDevices().clear();
+        getBluetoothDeviceNames().clear();
+        getBluetoothDevices().clear();
 
         IntentFilter filter = new IntentFilter();
 
@@ -99,12 +118,43 @@ public class Bluetooth {
         context.registerReceiver(mReceiver, filter);
     }
 
+    public Spinner getDeviceSpinner() {
+        spinner = (Spinner) activity.findViewById(R.id.spinner);
+        ArrayAdapter deviceAdapter = getDeviceList();
+
+        //Spinner that contains devices to connect to
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                //IF TOSTRING ERROR ARISES CHECK THIS
+                selectedDeviceName = spinner.getSelectedItem().toString();
+                Toast.makeText(context, selectedDeviceName, Toast.LENGTH_SHORT).show();
+            }
+
+            public void onNothingSelected(AdapterView parent) {
+                // Do nothing.
+            }
+        });
+
+        spinner.setAdapter(deviceAdapter);
+
+        return spinner;
+    }
+
+    //Returns the array adapter which contains the strings for the spinner
+    public ArrayAdapter getDeviceList() {
+        ArrayAdapter arrayAdapter = new ArrayAdapter<>(context,
+                android.R.layout.simple_spinner_item, getBluetoothDeviceNames());
+
+        return arrayAdapter;
+    }
+
     //Connect to device
     public void connect(BluetoothDevice device) {
         if (device != null) {
             try {
                 UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
-                BluetoothSocket btSocket = device.createRfcommSocketToServiceRecord(uuid);
+                btSocket = device.createRfcommSocketToServiceRecord(uuid);
                 btSocket.connect();
                 output = new PrintWriter(btSocket.getOutputStream());
                 workerThread.start();
@@ -120,46 +170,55 @@ public class Bluetooth {
 
     //Send instructions to the connected device
     public void send(String msg) {
-        output.println(msg);
-        output.flush();
+        try {
+            output.println(msg);
+            output.flush();
+        } catch (Exception e) {
+            Toast.makeText(context, "Failed to send", Toast.LENGTH_SHORT).show();
+        }
     }
 
     //This is the function that returns devices to us
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
 
         public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
+            try {
+                String action = intent.getAction();
 
-            //When discovery of devices starts
-            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-                //discovery starts, we can show progress dialog or perform other tasks
-                Toast.makeText(context, "Discovery started", Toast.LENGTH_SHORT).show();
+                //When discovery of devices starts
+                if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                    //discovery starts, we can show progress dialog or perform other tasks
+                    Toast.makeText(context, "Discovery started", Toast.LENGTH_SHORT).show();
 
-            //When discovery of devices finishes
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                //discovery finishes, dismis progress dialog
-                Toast.makeText(context, "Discovery finished", Toast.LENGTH_SHORT).show();
+                    //When discovery of devices finishes
+                } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                    //discovery finishes, dismis progress dialog
+                    Toast.makeText(context, "Discovery finished", Toast.LENGTH_SHORT).show();
+                    getDeviceSpinner();
 
-            //When a bluetooth device is foudn
-            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                //bluetooth device found
-                device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                //Below statements are made in order to prevent the program from crashing
-                if (device != null) {
-                    if (device.getName().length() > 0) {
-                        getFoundDevices().add(0, device);
-                        if (device.getName().equals("Avengers")) {
-                            //Add it to the top of the list in order to save time
-                            getFoundDeviceNames().add(0, device.getName());
-                            //Stop discovery of devices, our device has been found
-                            adapter.cancelDiscovery();
-                        } else {
-                            getFoundDeviceNames().add(device.getName());
+
+                    //When a bluetooth device is found
+                } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    //bluetooth device found
+                    device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    //Below statements are made in order to prevent the program from crashing
+                    if (device != null) {
+                        if (device.getName().length() > 0) {
+                            getBluetoothDevices().add(0, device);
+                            if (device.getName().equals("Avengers")) {
+                                //Add it to the top of the list in order to save time
+                                getBluetoothDeviceNames().add(0, device.getName());
+                                //Stop discovery of devices, our device has been found
+                                adapter.cancelDiscovery();
+                            } else {
+                                getBluetoothDeviceNames().add(0, device.getName());
+                            }
                         }
                     }
+                    //Shows a toast of a found device
+                    Toast.makeText(context, "Found device: " + device.getName(), Toast.LENGTH_SHORT).show();
                 }
-                //Shows a toast of a found device
-                Toast.makeText(context, "Found device: " + device.getName(), Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
             }
         }
     };
--- app/src/main/java/com/sahragard/myremote/MainActivity.java
+++ app/src/main/java/com/sahragard/myremote/MainActivity.java
@@ -2,113 +2,74 @@ package com.sahragard.myremote;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
     public TextView statusUpdate;
-    public Button connectButton;
-    public Button disconnectButton;
+    public Button enableBT;
+    public Button disableBT;
     public Button connectDevice;
-    public Spinner conSpinner;
-    ArrayAdapter<String> adapter;
-    String selectedDeviceName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Bluetooth bt = new Bluetooth(MainActivity.this, btAdapter);
-
-        /*
-
-        REMINDER
-        START DISCOVERY MUST BE TRIGGERED AFTER BLUETOOTH HAVE STARTED
-
-         */
+        final Bluetooth bt = new Bluetooth(MainActivity.this, btAdapter, MainActivity.this);
 
         //Displays bluetooth status in the top
         statusUpdate = (TextView) findViewById(R.id.statusUpdate);
 
-        //Spinner that contains devices to connect to
-        conSpinner = (Spinner) findViewById(R.id.spinner);
-        conSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                //IF TOSTRING ERROR ARISES CHECK THIS
-                selectedDeviceName = conSpinner.getSelectedItem().toString();
+        if (btAdapter.isEnabled()) {
+            statusUpdate.setText("Bluetooth on");
+            btAdapter.startDiscovery();
+            bt.findDevices();
 
-            }
-
-            public void onNothingSelected(AdapterView parent) {
-                // Do nothing.
-            }
-
-        });
-
-
+        } else {
+            statusUpdate.setText("Bluetooth off");
+        }
 
 
         //Start bluetooth and find devices
-        connectButton = (Button) findViewById(R.id.connect);
-        connectButton.setOnClickListener(new Button.OnClickListener() {
+        enableBT = (Button) findViewById(R.id.connect);
+        enableBT.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 if (!btAdapter.isEnabled()) {
+                    bt.enableBT();
 
-                    Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(enableBluetooth, 0);
+                    //Waits until bluetooth is enabled, otherwise discovery will not start
+                    while (!btAdapter.isEnabled()) {
+                    }
 
                     btAdapter.startDiscovery();
-
                     bt.findDevices();
-                    statusUpdate.setText("Bluetooth on");
-                }
-            }
-        });
-        Button searchButton = (Button) findViewById(R.id.searchButton);
-        searchButton.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                //adapter.clear();
-                //Adapter that hold device strings
-                adapter = new ArrayAdapter<String>(MainActivity.this,
-                        android.R.layout.simple_spinner_item, bt.getFoundDeviceNames());
 
-                conSpinner.setAdapter(adapter);
+
+
+                    statusUpdate.setText("Bluetooth on");
+                } else {
+                    Toast.makeText(MainActivity.this, "Bluetooth already enabled", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         //Disable bluetooth
-        disconnectButton = (Button) findViewById(R.id.disconnect);
-        disconnectButton.setOnClickListener(new Button.OnClickListener() {
+        disableBT = (Button) findViewById(R.id.disconnect);
+        disableBT.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-                btAdapter.disable();
-                adapter.clear();
+                bt.disableBT();
 
-                statusUpdate.setText("Bluetooth off");
                 //Clear list to prevent duplicate entries
-
+                bt.getDeviceList().clear();
+                statusUpdate.setText("Bluetooth off");
             }
         });
 
@@ -116,25 +77,15 @@ public class MainActivity extends AppCompatActivity {
         connectDevice = (Button) findViewById(R.id.connectDevice);
         connectDevice.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-                for (BluetoothDevice device : bt.getFoundDevices()) {
-                    if (device != null && device.getName().equals(selectedDeviceName)) {
-                        bt.connect(device);
+                for (BluetoothDevice device : bt.getBluetoothDevices()) {
+                    if (device != null) {
+                        if (device.getName().equals(bt.selectedDeviceName)) {
+                            bt.connect(device);
+                        }
                     }
                 }
             }
         });
-
-
-        if (btAdapter.isEnabled()) {
-            btAdapter.cancelDiscovery();
-            ;
-            btAdapter.startDiscovery();
-            statusUpdate.setText("Bluetooth on");
-            bt.findDevices();
-
-        } else {
-            statusUpdate.setText("Bluetooth off");
-        }
 
 
         final Button up = (Button) findViewById(R.id.upButton);
