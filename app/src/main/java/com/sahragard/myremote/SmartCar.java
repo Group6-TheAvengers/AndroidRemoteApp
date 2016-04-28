b454f2e73ba53b31fb2927cbba054043a13bc2a9
Asmatkhan <jilor-man@hotmail.com>
2016-04-28 12:24:14 +0200

    Joystick Update

---
+++ app/src/main/java/com/sahragard/myremote/SmartCar.java
@@ -0,0 +1,238 @@
+package com.sahragard.myremote;
+
+import android.widget.TextView;
+
+public class SmartCar {
+
+    private Bluetooth bt;
+    private int currentSpeed;
+    public static String currentDirection;
+    private int distance;
+    private String backOrForth;
+    private static TextView textView;
+
+    public SmartCar(final Bluetooth bt) {
+        this.bt = bt;
+        this.textView = textView;
+        currentDirection = "isNotMoving";
+        textView.setText(currentDirection);
+    }
+
+
+    public void moveForward(int speed) {
+        if(currentDirection == "isNotMoving" || currentDirection == "isForward") {
+            currentDirection = "isForward";
+            switch (speed) {
+                case 1:
+                    bt.send("a");
+                    System.out.println("------Working-----");
+                    break;
+                case 2:
+                    bt.send("b");
+                    System.out.println("------Working-----");
+                    break;
+                case 3:
+                    bt.send("c");
+                    System.out.println("------Working-----");
+                    break;
+            }
+            textView.setText(currentDirection);
+        }
+    }
+
+    public void moveBackward(int speed) {
+        if(currentDirection == "isNotMoving" || currentDirection == "isBackward") {
+            currentDirection = "isBackward";
+            switch (speed) {
+                case 1:
+                    bt.send("d");
+                    break;
+                case 2:
+                    bt.send("e");
+                    break;
+                case 3:
+                    bt.send("f");
+                    break;
+            }
+            textView.setText(currentDirection);
+        }
+    }
+
+    public void moveLeft(){
+        bt.send("l");
+
+    }
+
+    public void moveRight(){
+        bt.send("i");
+    }
+
+    public void moveForwardLeft(int speed){
+        if(currentDirection == "isNotMoving" || currentDirection == "isForward") {
+            currentDirection = "isForward";
+            switch (speed) {
+                case 1:
+                    bt.send("a");
+                    bt.send("j");
+                    break;
+                case 2:
+                    bt.send("b");
+                    bt.send("j");
+                    break;
+                case 3:
+                    bt.send("c");
+                    bt.send("b");
+                    break;
+            }
+        }
+    }
+
+    public void moveForwardLeftLeft(int speed){
+        if(currentDirection == "isNotMoving" || currentDirection == "isForward") {
+            currentDirection = "isForward";
+            switch (speed) {
+                case 1:
+                    bt.send("a");
+                    bt.send("k");
+                    break;
+                case 2:
+                    bt.send("b");
+                    bt.send("k");
+                    break;
+                case 3:
+                    bt.send("c");
+                    bt.send("k");
+                    break;
+            }
+            textView.setText(currentDirection);
+        }
+    }
+
+    public void moveForwardRight(int speed){
+        if(currentDirection == "isNotMoving" || currentDirection == "isForward") {
+            currentDirection = "isForward";
+            switch (speed) {
+                case 1:
+                    bt.send("a");
+                    bt.send("g");
+                    break;
+                case 2:
+                    bt.send("b");
+                    bt.send("g");
+                    break;
+                case 3:
+                    bt.send("c");
+                    bt.send("g");
+                    break;
+            }
+        }
+    }
+
+    public void moveForwardRightRight(int speed){
+        if(currentDirection == "isNotMoving" || currentDirection == "isForward") {
+            currentDirection = "isForward";
+            switch (speed) {
+                case 1:
+                    bt.send("a");
+                    bt.send("h");
+                    break;
+                case 2:
+                    bt.send("b");
+                    bt.send("h");
+                    break;
+                case 3:
+                    bt.send("c");
+                    bt.send("h");
+                    break;
+            }
+        }
+    }
+
+    public void moveBackwardLeft(int speed) {
+        if(currentDirection == "isNotMoving" || currentDirection == "isBackward") {
+            currentDirection = "isBackward";
+            switch (speed) {
+                case 1:
+                    bt.send("d");
+                    bt.send("j");
+                    break;
+                case 2:
+                    bt.send("e");
+                    bt.send("j");
+                    break;
+                case 3:
+                    bt.send("f");
+                    bt.send("j");
+                    break;
+            }
+        }
+    }
+
+    public void moveBackwardLeftLeft(int speed) {
+        if(currentDirection == "isNotMoving" || currentDirection == "isBackward") {
+            currentDirection = "isBackward";
+            switch (speed) {
+                case 1:
+                    bt.send("d");
+                    bt.send("k");
+                    break;
+                case 2:
+                    bt.send("e");
+                    bt.send("k");
+                    break;
+                case 3:
+                    bt.send("f");
+                    bt.send("k");
+                    break;
+            }
+        }
+    }
+
+    public void moveBackwardRight(int speed) {
+        if(currentDirection == "isNotMoving" || currentDirection == "isBackward") {
+            currentDirection = "isBackward";
+            switch (speed) {
+                case 1:
+                    bt.send("d");
+                    bt.send("g");
+                    break;
+                case 2:
+                    bt.send("e");
+                    bt.send("g");
+                    break;
+                case 3:
+                    bt.send("f");
+                    bt.send("g");
+                    break;
+            }
+        }
+    }
+
+    public void moveBackwardRightRight(int speed) {
+        if(currentDirection == "isNotMoving" || currentDirection == "isBackward") {
+            currentDirection = "isBackward";
+            switch (speed) {
+                case 1:
+                    bt.send("d");
+                    bt.send("h");
+                    break;
+                case 2:
+                    bt.send("e");
+                    bt.send("h");
+                    break;
+                case 3:
+                    bt.send("f");
+                    bt.send("h");
+                    break;
+            }
+        }
+    }
+
+    public void stop(){
+        currentDirection = "isNotMoving";
+        bt.send("m");
+
+    }
+
+}
+
